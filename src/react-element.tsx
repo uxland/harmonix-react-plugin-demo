f3e1e04367db1bf60981d9883dcd754efc271bc4
danielcabiscol <danielcabiscol@gmail.com>
2024-09-27 14:28:30 +0200

    chore: add styling scoped

--- src/react-element.tsx
+++ src/react-element.tsx
@@ -1,12 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import styles from './react-element.css?inline';
 
 // Tu componente de React
 const ComponenteBasico = () => {
+
+const [count, setCount] = React.useState(0);
+const increment = () => setCount(count + 1);
+const decrement = () => setCount(count - 1);
+
   return (
     <div>
+      <h1>Contador: {count}</h1>
+      <button onClick={increment}>Incrementar</button>
+      <button onClick={decrement}>Decrementar</button>
       <h1>¡Hola, mundo!</h1>
       <p>Este es un componente funcional básico en React dentro de un Web Component.</p>
+      <style type="text/css">{styles}</style>
     </div>
   );
 };
