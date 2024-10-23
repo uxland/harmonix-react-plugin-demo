e0afb70a4be92382a93efb3130fbc718a1ccb31b
Cristian Payet <payet91@gmail.com>
2024-10-23 11:59:24 +0000

    refactor

--- src/plugins/plugin-counter-css-modules/components/counter-button.tsx
+++
@@ -1,9 +0,0 @@
-import react, { ReactNode } from "react";
-import styles from './counter-button.module.css';
-export const CounterButton: react.FC<{children: ReactNode, onClick: () => void}> = ({children, onClick}) => {
-      return (
-        <button className={styles.button} onClick={onClick}>
-            {children}
-        </button>
-      );
-    };
