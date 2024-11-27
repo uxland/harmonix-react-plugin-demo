8a4ed6f88d3ed8879f89bbd2901726ac79d2fe78
Joan Egea <87185751+JE300@users.noreply.github.com>
2024-11-27 14:53:59 +0100

    Feat/vite config (#1)
    * updating vite config
    
    * config updated

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,44 @@
-import { defineConfig } from 'vite'
+// import { defineConfig } from 'vite'
 
-export default defineConfig({
-  server:{
-    host:'0.0.0.0'
-  }
-})
+// export default defineConfig({
+//   server:{
+//     host:'0.0.0.0'
+//   }
+// })
+
+import { defineConfig, loadEnv } from "vite";
+import pkg from "./package.json";
+export default ({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+  
+
+  return defineConfig({
+    define: {
+      'process.env': {}, // Define process.env para evitar el error
+    },
+    build: {
+      lib: {
+        // Could also be a dictionary or array of multiple entry points
+        entry: "./src/plugin.ts",
+        // the proper extensions will be added
+        fileName: "index",
+        name: pkg.name,
+        
+      },
+      
+      rollupOptions: {
+        external: ["react", "react-dom",  "@uxland/primary-shell"],
+        output: {
+          globals: {
+            lit: "lit",
+            "@uxland/primary-shell": "@uxland/primary-shell",
+          },
+          inlineDynamicImports: true, // Incluye todas las importaciones dinámicas en un solo archivo
+          manualChunks: undefined, // Desactiva la fragmentación para generar un solo archivo
+        }
+      },
+      minify: true,
+      sourcemap: true,
+    },
+  });
+};
