ec456aa5ca5a2e6041ed0ceb02aba427db87667a
Nacho Losada <ignaciolosadaprato@gmail.com>
2025-04-15 09:33:47 +0200

    feat: modify base plugin view

--- src/views/main/view.tsx
+++ src/views/main/view.tsx
@@ -1,22 +1,23 @@
 import React from 'react';
-import styles from './main.css?inline';
-import {CounterButton} from "../../components/counter-button/counter-button";
-import { wrapReactViewFactory } from '../../shared/adapter-react-web-component-factory';
+import { ActionButton } from "../../components/action-button/action-button";
+import { addItem, updateItem } from '../../activity-history-plugin-integration/activity-history-actions';
+import { PrimariaApi } from '@uxland/primary-shell';
 
-export const MainView = () => {
+interface MainViewProps {
+  api: PrimariaApi;
+}
 
-const [count, setCount] = React.useState(0);
-const increment = () => setCount(count + 1);
-const decrement = () => setCount(count - 1);
+export const MainView: React.FC<MainViewProps> = ({api}) => {
+
+const addHistoryItem = () => addItem(api);
+const modifyHistoryItem = () => updateItem(api);
 
   return (
     <>
       <h1>React Harmonix plugin</h1>
-      <h2>Contador: {count}</h2>
-      <CounterButton onClick={increment}>Incrementar</CounterButton>
-      <CounterButton onClick={decrement}>Decrementar</CounterButton>
-      <h1>¡Hola, mundo!</h1>
-      <p>Este es un componente funcional básico en React dentro de un Web Component.</p>
+      <ActionButton onClick={addHistoryItem}>Afegir Element</ActionButton>
+      <ActionButton onClick={modifyHistoryItem}>Modificar Element</ActionButton>
+      <p>Afegeix o modifica elements al segiment clinic.</p>
     </>
   );
 };
