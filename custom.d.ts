240e10a9c84ad8091b95bfeac4bedb1290700bd2
Joan Egea <joanegea300@gmail.com>
2025-05-15 14:13:55 +0200

    added custom d ts

---
+++ custom.d.ts
@@ -0,0 +1,11 @@
+declare namespace JSX {
+  interface IntrinsicElements {
+    // 'dss-button': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement> & {
+    //   onClick?: () => void;
+    //   label?: string;
+    //   size?: string; // o el tipo específico si lo conoces (ej: "sm" | "md" | "lg")
+    //   variant?: string; // o el tipo específico si lo conoces (ej: "primary" | "secondary")
+    // };
+    "dss-button": any;
+  }
+}
