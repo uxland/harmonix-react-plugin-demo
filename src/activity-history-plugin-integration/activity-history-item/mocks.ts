d48b0163871b03458339d0ab985d983596db0b57
danielcabiscol <danielcabiscol@gmail.com>
2025-05-16 11:15:08 +0200

    fix: correct diagnostics

--- src/activity-history-plugin-integration/activity-history-item/mocks.ts
+++ src/activity-history-plugin-integration/activity-history-item/mocks.ts
@@ -1,41 +1,42 @@
 export const mockActivityHistoryItem = {
-    id: 'ID12345 ',
-    date: "03/06/2024",
-    professional: {
-      id: 'professional456',
-      name: 'Dr. Martínez',
-      speciality: 
-      {
-        id: "string", 
-        description: "Metge família"
-      },
-      role: {
-        id: "string", 
-        description: "string"
-      },
-    },
-    relevant: true,
-    diagnostics: [
-      {
-        id: 'diagnostic789',
-        description: 'Gonàlgia',
-      },
-    ],
-    center: {
-      id: 'center321',
-      description: 'CAP Manso',
-    },
-    up: {
-      id: 'up654',
-      description: 'Unitat Emergències',
-    },
-    ep: {
-      id: 'ep987',
-      description: 'Pediatria',
-    },
-    service: {
-      id: 'service543',
-      description: 'Cardiología',
-    },
-  };
-  
+	id: "ID12345 ",
+	date: "03/06/2024",
+	professional: {
+		id: "professional456",
+		name: "Dr. Martínez",
+		speciality: {
+			id: "string",
+			description: "Metge família",
+		},
+		role: {
+			id: "string",
+			description: "string",
+		},
+	},
+	relevant: true,
+	diagnostics: [
+		{
+			idDiagnostic: "-1-0",
+			version: "1",
+			codi: "J01.90",
+			descripcio: "SINUSITIS AGUDA",
+			cataleg: "2.16.840.1.113883.6.3",
+		},
+	],
+	center: {
+		id: "center321",
+		description: "CAP Manso",
+	},
+	up: {
+		id: "up654",
+		description: "Unitat Emergències",
+	},
+	ep: {
+		id: "ep987",
+		description: "Pediatria",
+	},
+	service: {
+		id: "service543",
+		description: "Cardiología",
+	},
+};
