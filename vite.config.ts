549b3460b3de9ffe2737a73db422399f4db1600f
Xiaolin Jin Lin <dev@xiaojl.dev>
2026-07-30 17:58:55 +0200

    dev: type vite config and normalise formatting

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,14 @@
-import { defineConfig, loadEnv } from "vite";
+import { type ConfigEnv, defineConfig, loadEnv } from "vite";
 import pkg from "./package.json";
-export default ({ mode }) => {
-  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
-  
 
+export default ({ mode }: ConfigEnv) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
   return defineConfig({
     define: {
-      'process.env': {NODE_ENV: "production"}, // Define process.env para evitar el error
+      "process.env": { NODE_ENV: "production" }, // Define process.env para evitar el error
     },
-    server:{
-      host:'0.0.0.0'
+    server: {
+      host: "0.0.0.0",
     },
     build: {
       lib: {
@@ -18,14 +17,14 @@ export default ({ mode }) => {
         // the proper extensions will be added
         fileName: "index",
         name: pkg.name,
-        formats: ["es"]
+        formats: ["es"],
       },
       rollupOptions: {
-        external: ["react", "react-dom",  "@uxland/primary-shell", "@uxland/regions", "react-dom/client"],
+        external: ["react", "react-dom", "@uxland/primary-shell", "react-dom/client"],
         output: {
           inlineDynamicImports: true, // Incluye todas las importaciones dinámicas en un solo archivo
           manualChunks: undefined, // Desactiva la fragmentación para generar un solo archivo
-        }
+        },
       },
       minify: true,
       sourcemap: true,
